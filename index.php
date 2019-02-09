0ab8651acbcb7a8f77fe545e4930d0a78e025109
samatebintou1998 <moustapha.bintou.03@gmail.com>
2019-02-09 04:42:08 +0000

    insertion du style

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Ma page de test</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<img src="imge.jpg">
+	</header>
+	<p>
+		<h3>bienvenue</h3>
+		ATTENTION, LES PIRATES SONT COMPÉTENTS EN CRYPTOGRAPHIE…
+		<mark>Il faut remplirer ce formulaire pour se connecter</mark>
+	</p>
+
+	<form action = "traitement_f.php" method="post">
+		<strong>Votre nom :</strong> 
+		<input type = "text" name = "nom" value="Votre nom"><br />
+		<strong>votre prenom</strong>
+		<input type="text" name="prenom" value="votre prenom"><br>
+		<strong>Votre fonction :</strong>
+		<input type = "text" name = "fonction" value="etudiant"><br />
+		<input type = "submit" value = "connecter">
+	</form>
+
+	<footer>
+		pour plus information contact nous<br>
+		Egmail::bintoutapha2017@gmail.com ou bien // telephone ::785515990
+	</footer>
+</body>
+</html>
